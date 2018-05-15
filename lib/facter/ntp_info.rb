4bc00f3b609d57e408b213cda033c9a7a1811ac0
Samuel Keeley <samuel@airbnb.com>
2018-05-15 07:17:06 -0500

    cleanup fact

--- lib/facter/ntp_info.rb
+++ lib/facter/ntp_info.rb
@@ -1,18 +1,17 @@
 # ntp_info.rb
 Facter.add(:ntp_info) do
-    confine kernel: 'Darwin'
-    setcode do
-        out = {}
-        out['network_time'] = false
-        using_network_time = Facter::Util::Resolution.exec('/usr/sbin/systemsetup -getusingnetworktime')
+  confine kernel: 'Darwin'
+  setcode do
+    out = {}
+    out['network_time'] = false
+    using_network_time = Facter::Util::Resolution.exec('/usr/sbin/systemsetup -getusingnetworktime')
 
-        if using_network_time.gsub("Network Time: ","") == 'On'
-            out['network_time'] = true
-        end
-
-        out['network_time_server'] = Facter::Util::Resolution.exec('/usr/sbin/systemsetup -getnetworktimeserver').gsub("Network Time Server: ", "")
-
-        out
-        
+    if using_network_time.gsub('Network Time: ', '') == 'On'
+      out['network_time'] = true
     end
+
+    out['network_time_server'] = Facter::Util::Resolution.exec('/usr/sbin/systemsetup -getnetworktimeserver').gsub('Network Time Server: ', '')
+
+    out
   end
+end
